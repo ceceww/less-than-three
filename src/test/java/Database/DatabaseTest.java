20def95a760455b73344eaca82062fd64c12f9dc
Cecelia Wisniewska <ceceww@hotmail.com>
2020-01-15 15:50:48 +0000

    Add junit testing capability and DatabaseTest.java

---
+++ src/test/java/Database/DatabaseTest.java
@@ -0,0 +1,30 @@
+package Database;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void dbConnect() {
+    }
+
+    @Test
+    void createTable() {
+    }
+
+    @Test
+    void write() {
+    }
+}
